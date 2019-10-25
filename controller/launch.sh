44cc1c9d29758ea25af8f1791d62e1ca42857ae1
jumbatm <30644300+jumbatm@users.noreply.github.com>
2019-10-26 01:53:40 +1000

    Update with current parameters.

--- controller/launch.sh
+++ controller/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-export RUIP=http://localhost:3001
-export AWSBUCKETNAME=cab432
-export REDISCACHEHOSTNAME=my-redis-cache.redis.cache.windows.net
-export REDISCACHEKEY=JnmBrSnYJEC7e+6a+zXUtS4BaIAQT454Glza0BXlmOM=
+export RUIP="http://cloudpathtracerrenderingunitsingle.australiaeast.cloudapp.azure.com:3000"
+export AWSBUCKETNAME=cloud-path-tracer-jon
+export REDISCACHEHOSTNAME=cloud-path-tracer.redis.cache.windows.net
+export REDISCACHEKEY=Woq8QMs6+SF2SoDTURPqKz+vTs0mrnGvlBE4nGRec18=
 export CACHEPORT=6380
 npm start
