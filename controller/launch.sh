3de3679d6b93932f7b81c95afa021eee91d042b0
jumbatm <30644300+jumbatm@users.noreply.github.com>
2019-10-26 02:27:55 +1000

    Show the public bucket image if it's rendered.

--- controller/launch.sh
+++ controller/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-export RUIP="http://cloudpathtracerrenderingunitsingle.australiaeast.cloudapp.azure.com:3000"
-export AWSBUCKETNAME=cloud-path-tracer-jon
+export RUIP="http://ec2-13-236-153-77.ap-southeast-2.compute.amazonaws.com:3000"
+export AWSBUCKETNAME=cloud-path-tracer-tiles
 export REDISCACHEHOSTNAME=cloud-path-tracer.redis.cache.windows.net
-export REDISCACHEKEY=Woq8QMs6+SF2SoDTURPqKz+vTs0mrnGvlBE4nGRec18=
+export REDISCACHEKEY=AofcMYlgaeglj7V8yvhHgIqLurgxWNnEDA1NB7hO7X8=
 export CACHEPORT=6380
 npm start
